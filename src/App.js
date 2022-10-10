0bef9c110071af3f466d5848b6f1050d4ef2a6eb
Dementexo <browndon962@gmail.com>
2022-10-10 15:57:29 -0500

    History button/Send button added

--- src/App.js
+++ src/App.js
@@ -25,9 +25,15 @@ class App extends Component {
       ReceivedMessage: { text: '' },
     });
   };
+  deleteHistory = () => {
+    this.setState({
+      ReceivedMessage: {text: ''}, 
+      sentMessages: []
+    })
+  };
   render() {
     const { ReceivedMessage, sentMessages } = this.state;
-
+    
     return(
       <div className='App'>
         <div className='siteContainer'>
@@ -36,14 +42,14 @@ class App extends Component {
             <div className='dashBoard'>
               dashboard
             </div>
-            <div className='messageBox'>
+            <div className='messageBox' ref={ this.gmContainerRef }>
               <UiUpdate sentMessages = {sentMessages}/>
             </div>
           </div>
           <div className='chatAndTools'>
            <div className='shortcutOptions'>
               <div className='deleteHistory'>
-                <button className='dhButton'>
+                <button className='dhButton' onClick={this.deleteHistory}>
                   <img className='dhImg'src={require("./Images/history.png")} alt='Delete all history'></img>
                 </button>
               </div>
@@ -51,7 +57,9 @@ class App extends Component {
             <div className='chatBox'>
               <form className='inputForm' onSubmit={this.msgSubmission}>
                 <input className='chatInput' placeholder="Click enter to send" type="text" value={ReceivedMessage.text} onChange={this.handleChange}></input>
-                <button className='enterButton' type='submit'>Send</button>
+                <button className='enterButton' type='submit'>
+                  <img className='sendImg' src={require("./Images/send-message.png")} alt='Send Message'></img>
+                </button>
               </form>
             </div>
           </div>
