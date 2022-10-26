c257f81be8cd0039a5f964167bf13402fd7d9494
Dementexo <browndon962@gmail.com>
2022-10-25 23:29:14 -0500

    foundations for user search done. optimizing.

--- src/social.js
+++ src/social.js
@@ -7,25 +7,37 @@ import { db } from './firebase';
 export const UserListGeneration = () => {
 
     const [users, setUsers] = useState([]);
+    const [profileURL, setProfileURL] = useState([]);
 
     useEffect(() => {
         db.collection('Friends').get().then((querySnapshot) => {
             const collection = [];
+            const collection2 = [];
+
             querySnapshot.forEach((snapshot) => {
                 collection.push(snapshot.id);
+                collection2.push(snapshot.get("ProfilePicture"));
             })
-            setUsers(collection)
+            setUsers(collection);
+            setProfileURL(collection2);
+            console.log(collection2);
         })
     },[]);
 
     return(
         <div className="generatedUserContainer">
-            {users.map((selectedUser) => {
-                return <div className="generatedUser">
-                    {selectedUser}
-                    <img className="guImg" src={db.collection("Friends").doc(selectedUser).get().then((data) => {return data.get("ProfilePicture"), console.log(data.get("ProfilePicture"))})}></img>
-                </div>
-            })}
+            <div className="userColumn">
+                {users.map((selectedUser) => {
+                    return <div className="generatedUser">
+                        {selectedUser}
+                    </div>
+                })}
+            </div>
+            <div className="photoColumn">
+                {profileURL.map((selectedURL) => {
+                    return <img className="guImg" src={selectedURL}></img>
+                })}
+            </div>
         </div>
     )
 }
