a6eccd696f21dd00ddca8eddf2106daa31e4cd4e
Dementexo <browndon962@gmail.com>
2022-11-15 13:36:07 -0600

    testing mini profile feature

--- src/social.js
+++ src/social.js
@@ -4,23 +4,29 @@ import { useEffect, useState } from "react";
 import React from "react";
 import { db } from './firebase';
 
+
 export const UserListGeneration = () => {
 
     const [users, setUsers] = useState([]);
     const [profileURL, setProfileURL] = useState([]);
-
+    const [miniInfo, setMI] = useState([]);
+    
     useEffect(() => {
         db.collection('Friends').get().then((querySnapshot) => {
             const collection = [];
             const collection2 = [];
+            const collection3 = [];
 
             querySnapshot.forEach((snapshot) => {
                 collection.push(snapshot.id);
                 collection2.push(snapshot.get("ProfilePicture"));
+                const data = {Name: snapshot.id, PFP: snapshot.get("ProfilePicture")};
+                collection3.push(data);
             })
             setUsers(collection);
             setProfileURL(collection2);
-            console.log(collection2);
+            setMI(collection3);
+            console.log(miniInfo)
         })
     },[]);
 
@@ -37,9 +43,24 @@ export const UserListGeneration = () => {
                 {users.map((selectedUser) => {
                     return <div className="generatedUser">
                         {selectedUser}
+                        <div className="holder">
+                            {miniInfo.map((data) => {
+                                if(data.Name == selectedUser){
+                                    return <div className="miniPF">
+                                    <div className="mpfPFP">
+                                        <img src={data.PFP}></img>
+                                    </div>
+                                    <div className="mpfName">
+                                        {data.Name}
+                                    </div>
+                                </div>
+                                }
+                            })}
+                        </div>
                     </div>
                 })}
             </div>
         </div>
     )
 }
+
